68a3a017d6377da054ba5ba4fd51670a5e09f3a9
ktj <xowls0304@gmail.com>
2026-07-29 17:44:58 +0900

    20260729

--- src/main/java/net/datasa/web5/controller/async/AsyncRestController.java
+++ src/main/java/net/datasa/web5/controller/async/AsyncRestController.java
@@ -1,12 +1,12 @@
 package net.datasa.web5.controller.async;
 
 import lombok.extern.slf4j.Slf4j;
+import net.datasa.web5.domain.dto.async.StudentDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-//@RestController
+@RestController
 @Controller
 @Slf4j
 @RequestMapping("/api")
@@ -21,10 +21,61 @@ public class AsyncRestController {
 			- 클래스에 붙이면 해당 클래스 내 모든 메서드에 @ResponseBody 가 가본 적용
 	 */
 	// 1. concept.html -------------------------------------------
-	@ResponseBody
+//	@ResponseBody
 	@GetMapping("/basic")
-	public String basicResponse() {return "Hello, Fetch World!";}
+	public String basicResponse() {
+		return "Hello, Fetch World!";
+	}
 	// ResponseBody 가 있으면 return 부분 값이 HTML 로 그대로 문자열로 넘어감.
 	
+	// 2. text.html -------------------------------------------
+	@GetMapping("/text/send")
+	public void asyncResponse(@RequestParam("msg") String msg) {
+		log.debug("클라이언트가 보낸 텍스트: {}", msg);
+	}
+	
+//	@ResponseBody
+	@GetMapping("/text/receive")
+	public String textReceive() {
+		return "서버가 보낸 메시지";
+	}
+	
+//	@ResponseBody
+	@PostMapping("/text/exchange")
+	public String textExchange(
+			@RequestParam("n1") int n1,
+			@RequestParam("n2") int n2
+	) {
+		return "두 수의 합은 " + (n1 + n2) + "입니다.";
+	}
+	
+	// 3. object.html --------------------------------------
+	// @RequestBody : HTTP 요청의 본문에 담긴 데이터를 자바 객체로 변환해주는 Annotation
+	@PostMapping("/object/send")
+	public void objectSend(
+			@RequestBody StudentDTO student
+	) {
+		
+		log.debug("수신된 학생 정보: {}", student);
+	}
+	
+//	@ResponseBody
+	@GetMapping("/object/receive")
+	public StudentDTO receive() {
+		return new StudentDTO("손흥민", 33);
+	}
+	
+	@PostMapping("/object/exchange")
+	public ResponseEntity<?> exchange(
+			@RequestBody StudentDTO student
+	) {
+		log.debug("변경 전: {}", student);
+		if(student.name() == null || student.name().trim().isEmpty()) {
+			return ResponseEntity.badRequest()
+					.body("이름은 필수 입력 항목입니다.");
+		}
+		
+		return ResponseEntity.ok(new StudentDTO("홍길동", 100));
+	}
 	
 }
---
+++ src/main/java/net/datasa/web5/domain/dto/async/StudentDTO.java
@@ -0,0 +1,6 @@
+package net.datasa.web5.domain.dto.async;
+
+
+public record StudentDTO(String name, int age) {
+
+}
